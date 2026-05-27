1ed4985d1ad74fbbeb2a25c245f80fda6677ab17
Vibesdk <vibesdk-bot@cloudflare.com>
2026-05-27 09:47:56 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -78,7 +78,8 @@ function updateWranglerJsonc() {
 
 function runSetupCommands() {
     const commands = [
-    "bun add react-intersection-observer@^9.13.0"
+    "bun add react-intersection-observer@^9.13.0",
+    "bun add lucide-react@latest"
 ];
     
     if (commands.length === 0) {
